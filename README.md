79cefccf0d7df39f87f26db5f94b307a62a808de
seren.kim <seren.kim@kakaomobility.com>
2019-02-10 17:01:34 +0900

    크롤링 끝나면 expoert하도록 수정, config.ini파일 참조하도록 수정

--- README.md
+++ README.md
@@ -3,6 +3,5 @@ selenium을 이용한 open.go.kr 크롤링
 
 ## 실행 방법
 1. os에 맞는 driver 다운로드 (./chromedriver)
-2. craw.py의 USER_ID, PASSWORD 수정
+2. config.ini의 USER_ID, PASSWORD 수정
 3. > python craw.py 2019-01-01
-3. > python export.py
--- craw.py
+++ craw.py
@@ -5,6 +5,7 @@ import datetime
 import requests
 import os
 import errno
+import configparser
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -12,10 +13,14 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 
-USER_ID = 'USER_ID'
-PASSWORD = 'PASSWORD'
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+USER_ID = config['DEFAULT']['USER_ID']
+PASSWORD = config['DEFAULT']['PASSWORD']
 CHROME_DRIVER_PATH = "./chromedriver"
 BILL_DB = "./bill.db"
+OUTPUT_HTML = "./result.html"
 
 LOGIN_URI = 'https://www.open.go.kr/pa/member/openLogin/memberLogin.do'
 BILLING_LIST_URI = 'https://www.open.go.kr/pa/billing/openBilling/openBillingList.do'
@@ -196,7 +201,37 @@ while True:
 		break
 	next_page()
 
-print("finish!")
 driver.close()
+
+with open(OUTPUT_HTML, "w") as f:
+	html = ""
+	html += '<link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">'
+	html += '<table class="pure-table pure-table-horizontal">'
+	html += "<thead>"
+	html += "<tr>"
+	html += "<th>접수번호</th><th>접수일</th><th>제목</th><th>처리기관명</th><th>처리상태</th><th>처리일자</th><th>비고</th><th>공개내용</th><th>파일이름</th><th>수정일</th>"
+	html += "</tr>"
+	html += "</thead>"
+	html += "<tbody>"
+	with sqlite3.connect(BILL_DB) as conn:
+		cur = conn.cursor()
+		for row in cur.execute("SELECT * FROM bills order by id desc") :
+			html += "<tr>"
+			for m in range(0,10):
+				html += "<td>"
+				if m == 8 :
+					html+= '<a href="file:///'+os.getcwd()+'/'+str(row[2])+'/'+str(row[3])+'/'+str(row[m])+'">'+str(row[m])+'</a>'
+				else :
+					html+= str(row[m])
+				html += "</td>"
+			html += "</tr>"
+	html += "</tbody>"
+	f.write(html)
+
+print("finish!")
 exit()
+
+
+
+
 
